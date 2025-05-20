31bfc9e625f428fbe8d1146a4ab10d895c3992e8
Faizah Rizki Auliawati <frauliawati@gmail.com>
2025-05-21 03:20:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+
+# HARUS paling atas sebelum perintah Streamlit lain
+st.set_page_config(page_title="Prediksi Daya Listrik - CCPP", layout="centered")
+
 import pandas as pd
 import numpy as np
 import joblib
@@ -29,7 +33,6 @@ y_pred_all = model.predict(X)
 r2_val = r2_score(y, y_pred_all)
 
 # UI Streamlit
-st.set_page_config(page_title="Prediksi Daya Listrik - CCPP", layout="centered")
 st.title("🔌 Prediksi Daya Listrik Pembangkit Listrik Siklus Gabungan (CCPP)")
 st.markdown("Menggunakan **Gradient Boosting Regressor** berdasarkan kondisi lingkungan.")
 
